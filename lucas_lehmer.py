b6723a76cd3fbd6a67a2f07ed4e1245e96813c2c
Muhammad Maaz <mmaaz6004@gmail.com>
2025-12-21 18:23:56 -0500

    Lucas lehmer test for mersenne numbers

---
+++ lucas_lehmer.py
@@ -0,0 +1,11 @@
+def is_prime(x):
+    """
+    Tests if the number 2^x-1 is prime.
+    """
+    M = 2**x - 1
+    s = 4
+    for _ in range(x-2):
+        s = (s**2 - 2) % M
+    return s == 0
+
+print(is_prime(86243))
