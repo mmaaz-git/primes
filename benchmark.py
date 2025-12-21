04d93d1fb48acb05e29acb0c85afe35a713e858d
Muhammad Maaz <mmaaz6004@gmail.com>
2025-12-21 18:37:55 -0500

    benchmarking

---
+++ benchmark.py
@@ -0,0 +1,34 @@
+# trying to push the limits of the algorithms
+
+import agrawal_biswas, lucas_lehmer
+from time import time
+
+for num in [
+        31,
+        61,
+        89,
+        107,
+        127,
+        521,
+        607,
+        1279,
+        2203,
+        2281,
+        3217,
+        4253,
+        4423,
+        9689,
+        9941,
+        11213,
+        19937,
+        21701,
+        23209,
+        44497,
+        86243
+    ]:
+    print(f"Testing if 2^{num}-1 is prime...")
+    start = time()
+    res = lucas_lehmer.is_prime(num)
+    end = time()
+    print(f"result: {res}")
+    print(f"elapsed: {end-start:.2f} sec")
