0c7b0b2d54b66f2416f5c285b24d9fa355f2359e
Muhammad Maaz <mmaaz6004@gmail.com>
2025-12-21 18:22:19 -0500

    using my custom polynomial operations, no sympy

--- agrawal_biswas.py
+++ agrawal_biswas.py
@@ -1,27 +1,40 @@
-from utils import PRIMES_UPTO_1000, is_perfect_power, log2
+from utils import PRIMES_UPTO_1000, is_perfect_power, log2, poly_pow_modQ, poly_mul_modQ
 from math import ceil, floor
 import sympy as sp
+from sympy import ZZ
 import sympy.abc
 from sympy.polys import Poly
-from random import randint
+from random import randrange
 
 def is_prime(x):
     if x in PRIMES_UPTO_1000: return True
     if any(x % n == 0 for n in PRIMES_UPTO_1000): return False
     if is_perfect_power(x): return False
 
-    # form the polynomial P(z) = (1+z)^x - 1 - z^x
-    z = sp.abc.z
-    P = (1+z)**x - 1 - z**x
-
-    # now form the random polynomial Q(z)
+    # form the random polynomial Q(z)
     # it should be monic, degree ceil(log x) with coeffs in Z_x
-    Q_deg = ceil(log2(x))
-    Q = z**Q_deg + sum(z**n * randint(0,x) for n in range(Q_deg))
+    deg = ceil(log2(x))
+    Q = [1] + [randrange(x) for _ in range(deg-1)]
 
-    # if P divides Q over Z_n then prime, else composite
-    rem = Poly(P,z).rem(Poly(Q,z))
-    return all(c % x == 0 for c in rem.coeffs())
+    # form polys for P(z)
+    one = [0]*deg
+    one[0] = 1
+
+    z = [0]*deg
+    if deg > 1:
+        z[1] = 1
+
+    base = one[:]
+    if deg > 1:
+        base[1] = 1   # 1 + z
+
+    lhs = poly_pow_modQ(base, x, Q, x)
+    rhs = one[:]
+    zn  = poly_pow_modQ(z, x, Q, x)
+    for i in range(deg):
+        rhs[i] = (rhs[i] + zn[i]) % x
+
+    return lhs==rhs
 
 
 
