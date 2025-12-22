0a79c679d4352e01601efdbe5941b418579f6b9c
Muhammad Maaz <mmaaz6004@gmail.com>
2025-12-21 22:05:56 -0500

    using gmp for lucas lehmer now

--- lucas_lehmer.py
+++ lucas_lehmer.py
@@ -1,13 +1,19 @@
 import agrawal_biswas
 from utils import mersenne_reduce
+import gmpy2
+from gmpy2 import mpz, square
 
 def is_prime(x):
     """
     Tests if the number 2^x-1 is prime.
     Requires x to be prime. I don't check that for now, as I am just trying to reproduce known Mersenne primes.
     """
-    M = 2**x - 1
-    s = 4
-    for _ in range(x-2):
-        s = mersenne_reduce(s*s-2, x, M)
+    M = (mpz(1) << x) - 1
+    s = mpz(4)
+
+    for _ in range(x - 2):
+        s = square(s)
+        s -= 2
+        s %= M
+
     return s == 0
